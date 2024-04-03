79cef61db6490dbe24461274a3a795a5e62b034d
stanleyedward <stanleyedward9715@gmail.com>
2024-04-03 22:48:49 +0530

    sphere and voxel model arch

--- model.py
+++ model.py
@@ -64,3 +64,72 @@ class Nerf(nn.Module):
         
     def intersect(self, x, direction):
         return self.forward(x, direction)
+    
+
+class Sphere():
+    def __init__(self, position, radius, color):
+        self.position = position
+        self.radius = radius
+        self.color = color
+        
+    def intersect(self, x): #tells if ray hits the sphere or not
+        """_summary_
+
+        Args:
+            x (parameter): points shape [batch_size, 3]
+
+        Returns:
+            _type_: _description_
+        """
+        # check if inside the sphere
+        # (x-xo)^2 + (y-yo)^2 + (z-zo)^2 < r^2
+        condition =(x[:, 0] - self.position[0])**2 + (x[:, 1] - self.position[1])**2 + (x[:, 2] - self.position[2])**2 <= self.radius**2
+        
+        number_of_rays = x.shape[0]
+        colors = torch.zeros((number_of_rays, 3))
+        density = torch.zeros((number_of_rays, 1))
+
+        colors[condition] = self.color #assign color where condition passes
+        density[condition] = 10 # assume constant
+        
+        #creating a partition on the sphere for visuals
+        color_condition = x[:, 0] < 0
+        temp = colors[color_condition, 0].clone()
+        colors[color_condition, 0] = colors[color_condition, 1].clone()
+        colors[color_condition, 1] = temp
+        
+        return colors, density
+     
+    
+class Voxels(nn.Module):
+    def __init__(self, nb_voxels:int = 100, scale = 1, device = 'cpu'):
+        super(Voxels, self).__init__()
+        
+        self.voxels = torch.nn.Parameter(torch.rand((nb_voxels, nb_voxels, nb_voxels, 4),
+                                                    device = device, requires_grad=True)) #colors and density cannot be negative therefore dont use normal distribtion
+        
+        self.nb_voxels = nb_voxels
+        self.device = device
+        self.scale = scale
+        
+    def forward(self, xyz):
+        x = xyz[:, 0]
+        y = xyz[:, 1]
+        z = xyz[:, 2]
+     
+        condition = (x.abs() < (self.scale / 2)) & (y.abs() < (self.scale / 2)) & (z.abs() < (self.scale / 2))
+
+        colors_and_densities = torch.zeros((xyz.shape[0], 4), device=xyz.device)
+        
+        idx_x = (x[condition] / (self.scale / self.nb_voxels) + self.nb_voxels / 2).type(torch.long) 
+        idx_y = (y[condition] / (self.scale / self.nb_voxels) + self.nb_voxels / 2).type(torch.long) 
+        idx_z = (z[condition] / (self.scale / self.nb_voxels) + self.nb_voxels / 2).type(torch.long) 
+        
+        colors_and_densities[condition, :3] = self.voxels[idx_x, idx_y, idx_z, :3] 
+        colors_and_densities[condition, -1] = self.voxels[idx_x, idx_y, idx_z, -1]  
+        
+        return torch.sigmoid(colors_and_densities[:, :3]), torch.relu(colors_and_densities[:, -1:]) 
+    
+    def intersect(self, x):
+        return self.forward(x)
+    
