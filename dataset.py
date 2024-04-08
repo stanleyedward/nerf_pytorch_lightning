e84bc4a48bf530ba9d2b8d1b00b7ba0064577b70
stanleyedward <stanleyedward9715@gmail.com>
2024-04-09 01:00:08 +0530

    finish dataset

--- dataset.py
+++ dataset.py
@@ -1,106 +1,129 @@
 from torch.utils.data import Dataset
 import torch
 import numpy as np
-from utils import ray_generator
+from utils import get_ray_directions, get_rays
 import os
 import json
 from torchvision import transforms
 from PIL import Image
 
+
 class LegoDataset(Dataset):
-    def __init__(self, root_dir, split='train', img_shape=(800, 800), white_background:bool = True):
-        assert img_shape[0] == img_shape[1] , 'image width must equal image height'
-        
+    def __init__(
+        self,
+        root_dir,
+        split="train",
+        img_shape=(800, 800),
+        white_background: bool = True,
+    ):
+        assert img_shape[0] == img_shape[1], "image width must equal image height"
+
         self.root_dir = root_dir
         self.split = split
-        
+
         self.img_shape = img_shape
         self.white_background = white_background
-        
+
         self.read_meta()
         self.transform = transforms.ToTensor()
-        
+
     def read_meta_data(self):
-        with open(os.path.join(self.root_dir,
-                               f"transforms_{self.split}.json"), 'r') as file:
+        with open(
+            os.path.join(self.root_dir, f"transforms_{self.split}.json"), "r"
+        ) as file:
             self.meta = json.load(file)
-        
-        self.focal = 0.5*800/np.tan(0.5*self.meta['camera_angle_x'])
-        self.focal *= self.img_shape[0]/800
-        
+
+        self.focal = 0.5 * 800 / np.tan(0.5 * self.meta["camera_angle_x"])
+        self.focal *= self.img_shape[0] / 800
+
         self.tn = 2.0
         self.tf = 6.0
-        
-        self.direction = get_ray_directions(self.img_shape[0], self.img_shape[1], self.focal) # [H,W,3]
-        
-        if self.split == 'train':
+
+        self.directions = get_ray_directions(
+            self.img_shape[0], self.img_shape[1], self.focal
+        )  # [H,W,3]
+
+        if self.split == "train":
             self.all_rays = []
             self.all_rgbs = []
             self.valid_masks = []
-            
-            for frame in self.meta['frames']:
-                camera2world = torch.FloatTensor(frame['transform_matrix'])[:3, :4]
-                
+
+            for frame in self.meta["frames"]:
+                camera2world = torch.FloatTensor(frame["transform_matrix"])[:3, :4]
+
                 img_path = os.join(self.root_dir, f"{frame['file_path)']}.png")
                 img = Image.open(img_path)
-                img = self.transform(img) #[4, H, W]
-                _valid_mask = (img[-1] > 0).flatten() # [H*W] valid color area
+                img = self.transform(img)  # [4, H, W]
+                _valid_mask = (img[-1] > 0).flatten()  # [H*W] valid color area
                 self.valid_masks += [_valid_mask]
-                img = img.view(4, -1).permute(1, 0) # rgba [H*W, 4] 
-                img = img[:, :3]*img[:, -1:] + (1-img[:, -1:]) #blend a to rgbi
+                img = img.view(4, -1).permute(1, 0)  # rgba [H*W, 4]
+                img = img[:, :3] * img[:, -1:] + (1 - img[:, -1:])  # blend a to rgbi
                 self.all_rgbs += [img]
-                
-                rays_origin, rays_direction = get_rays(self.direction, camera2world)
-                self.all_rays += [torch.cat([rays_origin, rays_direction,
-                                             self.tn*torch.ones_like(rays_origin[:, :1]),
-                                             self.tf*torch.ones_like(rays_origin[:, :1])],
-                                            1)]
-            
-            self.all_rays = torch.cat(self.all_rays, 0) #[len(meta['frames'])*H*W, 3]
-            self.all_rgbs = torch.cat(self.all_rgbs, 0) #[len(meta['frames'])*H*W, 3]
-            self.valid_masks = torch.cat(self.valid_masks, 0) #[len(meta['frames'])*H*W]
-            
+
+                rays_origin, rays_direction = get_rays(self.directions, camera2world)
+                self.all_rays += [
+                    torch.cat(
+                        [
+                            rays_origin,
+                            rays_direction,
+                            self.tn * torch.ones_like(rays_origin[:, :1]),
+                            self.tf * torch.ones_like(rays_origin[:, :1]),
+                        ],
+                        1,
+                    )
+                ]
+
+            self.all_rays = torch.cat(self.all_rays, 0)  # [len(meta['frames'])*H*W, 3]
+            self.all_rgbs = torch.cat(self.all_rgbs, 0)  # [len(meta['frames'])*H*W, 3]
+            self.valid_masks = torch.cat(
+                self.valid_masks, 0
+            )  # [len(meta['frames'])*H*W]
+
     def reduce_to_valid(self):
         self.all_rays = self.all_rays[self.valid_masks]
         self.all_rgbs = self.all_rgbs[self.valid_masks]
-            
-    def __len__():
-        ...
-    
+
+    def __len__(self):
+        if self.split == "train":
+            return len(self.all_rays)
+        if self.split == "val":
+            return 1  # valid single image
+
+        return len(self.meta["frames"])
+
     def __getitem__(self, idx):
-        ...
-    
-    
+        if self.split == "train":  # use data in buffers
+            sample = {"rays": self.all_rays[idx], "rgbs": self.all_rgbs[idx]}
 
-def get_ray_directions(height:int, width:int, focus:int) -> np.ndarray:
-    rays_origin: np.ndarray = np.zeros((height * width, 3))
-    rays_direction: np.ndarray = np.zeros((height * width, 3)) 
-    
-    #coord grid
-    u: np.ndarray = np.arange(width)
-    v: np.ndarray = np.arange(height)
-    u,v = np.meshgrid(u, v) # u.shape & v.shape = [H, W]
-    
-    #direction
-    x: np.ndarray = u - (width/2)
-    y: np.ndarray = v - (height/2)
-    z: np.ndarray = np.ones_like(u) * focus
-    directions: np.ndarray = np.stack((x,
-                     -y, #-ve as we want y downwards
-                     -z), axis=-1) # -ve as were looking down throught -z axis
-    
-    return directions
+        else:  # create data for each img sperately
+            frame = self.meta["frames"][idx]
+            camera2world = torch.FloatTensor(frame["transform_matrix"])[:3, :4]
 
-def get_rays(height:int, width:int, directions:np.ndarray, camera2world:np.ndarray = None):
-    rays_origin: np.ndarray = np.zeros((height * width, 3))
-    rays_direction: np.ndarray = np.zeros((height * width, 3)) 
-    
-    if camera2world is not None:
-        directions = (camera2world[:3, :3] @ directions[..., None]).squeeze(-1)
-        rays_origin += camera2world[:3, 3]
-        
-    #normalization
-    rays_direction = directions / np.linalg.norm(directions, axis=-1, keepdims=True)
-    rays_direction = rays_direction.reshape(-1, 3)
-    
-    return rays_origin, rays_direction
+            img_path = os.path.join(self.root_dir, f"{frame['file_path']}.png")
+            img = Image.open(img_path)
+            img = img.resize(self.img_shape, Image.LANCZOS)
+            img = self.transform(img)  # [4, H, w]
+            valid_mask = (img[-1] > 0).flatten()  # [H*W] valid color region
+            img = img.view(4, -1).permute(1, 0)  # [H*W, 4] RGBA
+            img = img[:, :3] * img[:, -1:] + (1 - img[:, -1:])  # blend A to RGB
+
+            rays_origin, rays_direction = get_rays(self.directions, camera2world)
+
+            rays = torch.cat(
+                [
+                    rays_origin,
+                    rays_direction,
+                    self.tn * torch.ones_like(rays_origin[:, :1]),
+                    self.tf * torch.ones_like(rays_origin[:, :1]),
+                ],
+                1,
+            )
+
+            sample = {
+                "rays": rays,
+                "rgbs": img,
+                "c2w": camera2world,
+                "valid_mask": valid_mask,
+            }
+
+        return sample
--- utils.py
+++ utils.py
@@ -4,61 +4,98 @@ from pathlib import Path
 import torch
 from typing import Tuple
 
-def ray_generator(height:int, width:int, focus:int, camera2world=None) -> Tuple[np.ndarray, np.ndarray]:
+
+def ray_generator(
+    height: int, width: int, focus: int, camera2world=None
+) -> Tuple[np.ndarray, np.ndarray]:
     rays_origin: np.ndarray = np.zeros((height * width, 3))
-    rays_direction: np.ndarray = np.zeros((height * width, 3)) 
-    
-    #coord grid
+    rays_direction: np.ndarray = np.zeros((height * width, 3))
+
+    # coord grid
     u: np.ndarray = np.arange(width)
     v: np.ndarray = np.arange(height)
-    u,v = np.meshgrid(u, v) # u.shape & v.shape = [H, W]
-    
-    #direction
-    x: np.ndarray = u - (width/2)
-    y: np.ndarray = v - (height/2)
+    u, v = np.meshgrid(u, v)  # u.shape & v.shape = [H, W]
+
+    # direction
+    x: np.ndarray = u - (width / 2)
+    y: np.ndarray = v - (height / 2)
     z: np.ndarray = np.ones_like(u) * focus
-    dirs: np.ndarray = np.stack((x,
-                     -y, #-ve as we want y downwards
-                     -z), axis=-1) # -ve as were looking down throught -z axis
-    
+    dirs: np.ndarray = np.stack(
+        (x, -y, -z), axis=-1  # -ve as we want y downwards
+    )  # -ve as were looking down throught -z axis
+
     if camera2world is not None:
         dirs = (camera2world[:3, :3] @ dirs[..., None]).squeeze(-1)
         rays_origin += camera2world[:3, 3]
-        
-    #normalization
+
+    # normalization
     rays_direction = dirs / np.linalg.norm(dirs, axis=-1, keepdims=True)
     rays_direction = rays_direction.reshape(-1, 3)
-    
+
     return rays_origin, rays_direction
 
 
 def plot_rays(origin, direction, t) -> None:
     fig = plt.figure(figsize=(12, 12))
-    ax = fig.add_subplot(projection='3d')
-    
+    ax = fig.add_subplot(projection="3d")
+
     pt1 = origin
-    pt2 = origin + t * direction 
-    
-    for p1, p2 in zip(pt1[::50], pt2[::50]): #plot 1/50
+    pt2 = origin + t * direction
+
+    for p1, p2 in zip(pt1[::50], pt2[::50]):  # plot 1/50
         plt.plot([p1[0], p2[0]], [p1[1], p2[1]], [p1[2], p2[2]])
-    
-    
-    plt.xlabel('$X$ - coord')
-    plt.ylabel('$Y$ - coord')
-    
+
+    plt.xlabel("$X$ - coord")
+    plt.ylabel("$Y$ - coord")
+
     plt.show()
-    
-def save_model(model:torch.nn.Module, target_dir:str, model_name:str) -> None:
-    
+
+
+def save_model(model: torch.nn.Module, target_dir: str, model_name: str) -> None:
+
     target_dir_path: Path = Path(target_dir)
-    target_dir_path.mkdir(parents=True,
-                          exist_ok=True)
-    
-    assert model_name.endswith(".pth") or model_name.endswith(".pt"), "model_name should end with '.pth' or '.pt'"
+    target_dir_path.mkdir(parents=True, exist_ok=True)
+
+    assert model_name.endswith(".pth") or model_name.endswith(
+        ".pt"
+    ), "model_name should end with '.pth' or '.pt'"
     model_save_path: Path = target_dir_path / model_name
-    
-    #save
+
+    # save
     print(f"[INFO] saving model to : {model_save_path}")
-    torch.save(obj=model.state_dict(),
-               f=model_save_path)
-    
+    torch.save(obj=model.state_dict(), f=model_save_path)
+
+
+def get_ray_directions(height: int, width: int, focus: int) -> np.ndarray:
+    # coord grid
+    u: np.ndarray = np.arange(width)
+    v: np.ndarray = np.arange(height)
+    u, v = np.meshgrid(u, v)  # u.shape & v.shape = [H, W]
+
+    # direction
+    x: np.ndarray = u - (width / 2)
+    y: np.ndarray = v - (height / 2)
+    z: np.ndarray = np.ones_like(u) * focus
+
+    directions: np.ndarray = np.stack(
+        (x, -y, -z), axis=-1  # -ve as we want y downwards
+    )  # -ve as were looking down throught -z axis
+
+    return directions
+
+
+def get_rays(
+    height: int, width: int, directions: np.ndarray, camera2world: np.ndarray = None
+):
+    rays_origin: np.ndarray = np.zeros((height * width, 3))
+    rays_direction: np.ndarray = np.zeros((height * width, 3))
+
+    if camera2world is not None:
+        directions = (camera2world[:3, :3] @ directions[..., None]).squeeze(-1)
+        rays_origin += camera2world[:3, 3]
+
+    # normalization
+    rays_direction = directions / np.linalg.norm(directions, axis=-1, keepdims=True)
+    rays_direction = rays_direction.reshape(-1, 3)
+
+    return rays_origin, rays_direction
