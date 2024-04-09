f6673c5bc41df49e5d460bb9c4ea2ca84376c1f7
stanleyedward <stanleyedward9715@gmail.com>
2024-04-09 17:43:00 +0530

    remove annotations for testing

--- utils.py
+++ utils.py
@@ -5,36 +5,6 @@ import torch
 from typing import Tuple
 
 
-def ray_generator(
-    height: int, width: int, focus: int, camera2world=None
-) -> Tuple[np.ndarray, np.ndarray]:
-    rays_origin: np.ndarray = np.zeros((height * width, 3))
-    rays_direction: np.ndarray = np.zeros((height * width, 3))
-
-    # coord grid
-    u: np.ndarray = np.arange(width)
-    v: np.ndarray = np.arange(height)
-    u, v = np.meshgrid(u, v)  # u.shape & v.shape = [H, W]
-
-    # direction
-    x: np.ndarray = u - (width / 2)
-    y: np.ndarray = v - (height / 2)
-    z: np.ndarray = np.ones_like(u) * focus
-    dirs: np.ndarray = np.stack(
-        (x, -y, -z), axis=-1  # -ve as we want y downwards
-    )  # -ve as were looking down throught -z axis
-
-    if camera2world is not None:
-        dirs = (camera2world[:3, :3] @ dirs[..., None]).squeeze(-1)
-        rays_origin += camera2world[:3, 3]
-
-    # normalization
-    rays_direction = dirs / np.linalg.norm(dirs, axis=-1, keepdims=True)
-    rays_direction = rays_direction.reshape(-1, 3)
-
-    return rays_origin, rays_direction
-
-
 def plot_rays(origin, direction, t) -> None:
     fig = plt.figure(figsize=(12, 12))
     ax = fig.add_subplot(projection="3d")
@@ -66,18 +36,18 @@ def save_model(model: torch.nn.Module, target_dir: str, model_name: str) -> None
     torch.save(obj=model.state_dict(), f=model_save_path)
 
 
-def get_ray_directions(height: int, width: int, focus: int) -> np.ndarray:
+def get_ray_directions(height: int, width: int, focus: int):
     # coord grid
-    u: np.ndarray = np.arange(width)
-    v: np.ndarray = np.arange(height)
+    u = np.arange(width)
+    v = np.arange(height)
     u, v = np.meshgrid(u, v)  # u.shape & v.shape = [H, W]
 
     # direction
-    x: np.ndarray = u - (width / 2)
-    y: np.ndarray = v - (height / 2)
-    z: np.ndarray = np.ones_like(u) * focus
+    x = u - (width / 2)
+    y = v - (height / 2)
+    z = np.ones_like(u) * focus
 
-    directions: np.ndarray = np.stack(
+    directions = np.stack(
         (x, -y, -z), axis=-1  # -ve as we want y downwards
     )  # -ve as were looking down throught -z axis
 
@@ -85,17 +55,18 @@ def get_ray_directions(height: int, width: int, focus: int) -> np.ndarray:
 
 
 def get_rays(
-    height: int, width: int, directions: np.ndarray, camera2world: np.ndarray = None
-):
-    rays_origin: np.ndarray = np.zeros((height * width, 3))
-    rays_direction: np.ndarray = np.zeros((height * width, 3))
+    height: int, width: int, directions, camera2world = None
+) :
+    rays_origin = np.zeros((height * width, 3))
+    rays_direction = np.zeros((height * width, 3))
 
     if camera2world is not None:
         directions = (camera2world[:3, :3] @ directions[..., None]).squeeze(-1)
-        rays_origin += camera2world[:3, 3]
+        rays_origin += camera2world[:3, 3].numpy()
+            
 
     # normalization
     rays_direction = directions / np.linalg.norm(directions, axis=-1, keepdims=True)
     rays_direction = rays_direction.reshape(-1, 3)
 
-    return rays_origin, rays_direction
+    return torch.tensor(rays_origin), torch.tensor(rays_direction)
