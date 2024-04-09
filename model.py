800fd2653734acff1bcb886b65b7d4eb87c9201e
stanleyedward <stanleyedward9715@gmail.com>
2024-04-10 00:11:03 +0530

    scheduler + steps + formatting

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ import lightning as L
 from loss import mse2psnr, mse_loss
 from rendering import rendering
 
+
 class NeRFLightning(L.LightningModule):
     def __init__(self, learning_rate, tn, tf, nb_bins):
         super().__init__()
@@ -12,28 +13,56 @@ class NeRFLightning(L.LightningModule):
         self.tn = tn
         self.tf = tf
         self.nb_bins = nb_bins
-        
+
     def forward(self, xyz, direction):
         return self.nerf.forward(xyz, direction)
-    
+
     def intersect(self, x, direction):
         return self.nerf.forward(x, direction)
-    
-    def training_step(self, batch, batch_idx):
-        ...
-    
+
     def _common_step(self, batch, batch_idx):
         rays_origin = batch[:, :3]
         rays_direction = batch[:, 3:6]
         target_img = batch[:, 6:]
-        
-        pred = rendering(self, rays_origin, rays_direction, self.tn, self.tf, self.nb_bins, device='cuda')
-        
-    
+
+        pred = rendering(
+            self.nerf,
+            rays_origin,
+            rays_direction,
+            self.tn,
+            self.tf,
+            self.nb_bins,
+            device="cuda",
+        )
+        loss = mse_loss(pred, target_img)
+
+        return loss
+
+    def training_step(self, batch, batch_idx):
+        loss = self._common_step(batch, batch_idx)
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        loss = self._common_step(batch, batch_idx)
+        return loss
+
+    def test_step(self, batch, batch_idx):
+        loss = self._common_step(batch, batch_idx)
+        return loss
+
     def configure_optimizers(self):
-        return torch.optim.Adam(params=self.nerf.parameters(), lr=self.learning_rate)
-    
-    
+        optimizer = torch.optim.Adam(
+            params=self.nerf.parameters(), lr=self.learning_rate
+        )
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=[5, 10], gamma=0.5
+        )
+        return optimizer
+
+    def on_train_epoch_end(self) -> None:
+        self.scheduler.step()
+
+
 class Nerf(nn.Module):
     def __init__(self, L_position=10, L_direction=4, hidden_dim=256):
         super(Nerf, self).__init__()
