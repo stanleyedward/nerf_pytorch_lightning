b41f258ba982c62d0169390480e3b7a8848ddeef
stanleyedward <stanleyedward9715@gmail.com>
2024-04-12 18:36:34 +0530

    fix mse2psnr conversion

--- loss.py
+++ loss.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 def mse2psnr(mse_loss):
-    return 20 * np.log10(1 / np.sqrt(mse_loss))
+    return 20 * np.log10(1 / np.sqrt(mse_loss.cpu().detach()))
 
 
 def mse_loss(image, target_image):
