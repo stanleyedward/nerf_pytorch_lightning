047830c995e0ec8cd9c8f7f00c704c0288140763
stanleyedward <stanleyedward9715@gmail.com>
2024-04-13 14:38:40 +0530

    type annotation

--- config.py
+++ config.py
@@ -1,33 +1,38 @@
-HEIGHT = 400
-WIDTH = 400
-FOCUS = 1200
+import torch
 
-tn = 0.8
-tf = 1.2
 
-ORIGIN = [0.0, 0.0, -1.0]
-RADIUS = [0.1]
-learning_rate = 2e-1
-
-color = [0.0, 1.0, 1.0]  # cyan
-target_color = [1.0, 0.0, 1.0]  # purple
-
-SAVE_DIR = "images"
-
+"""------------------------NeRF Config------------------------"""
 # data
-DATA_DIR = "dataset/lego/"
-IMG_SIZE = 400
-BATCH_SIZE = 1024
+DATA_DIR:str = "dataset/lego/"
+IMG_SIZE:int = 400
+BATCH_SIZE:int = 1024
 
 # model
-LEARNING_RATE = 5e-4
-TN = 2
-TF = 6
-NB_BINS = 100
-GAMMA = 0.5
+LEARNING_RATE:float = 5e-4
+TN:float = 2.0
+TF:float = 6.0
+NB_BINS:int = 100
+GAMMA:float = 0.5
 
 # trainer
-ACCELERATOR = "gpu"
-DEVICES = [0]  # int(torch.cuda.isavailable())
-MAX_EPOCHS = 16
-PRECISION = "32"
+ACCELERATOR:str = "gpu"
+DEVICES:int = torch.cuda.device_count()
+MAX_EPOCHS:int = 16
+PRECISION:str = "32"
+STRATEGY:str = "ddp"
+
+
+"""------------------------Sphere Config------------------------"""
+# HEIGHT:int = 400
+# WIDTH:int = 400
+# FOCUS:int = 1200
+
+# tn:float = 0.8
+# tf:float = 1.2
+
+ORIGIN:list = [0.0, 0.0, -1.0]
+RADIUS:list = [0.1]
+# learning_rate:float = 2e-1
+
+color:list = [0.0, 1.0, 1.0]  # cyan
+target_color:list = [1.0, 0.0, 1.0]  # purple
--- loss.py
+++ loss.py
@@ -1,9 +1,19 @@
-import numpy as np
+from typing import Union
+import torch
 
+def mse2psnr(mse_loss: Union[float, torch.Tensor]) -> Union[float, torch.Tensor]:
+    return 20 * torch.log10(1 / torch.sqrt(mse_loss.cpu().detach()))
 
-def mse2psnr(mse_loss):
-    return 20 * np.log10(1 / np.sqrt(mse_loss.cpu().detach()))
-
-
-def mse_loss(image, target_image):
+def mse_loss(image: torch.Tensor, target_image: torch.Tensor) -> torch.Tensor:
     return ((image - target_image) ** 2).mean()
+
+# import numpy as np
+
+
+# def mse2psnr(mse_loss):
+#     return 20 * np.log10(1 / np.sqrt(mse_loss.cpu().detach()))
+
+
+# def mse_loss(image, target_image):
+#     return ((image - target_image) ** 2).mean()
+
